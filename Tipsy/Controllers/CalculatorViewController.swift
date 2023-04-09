22997da84be100e2f4f407172505a0d39f4eac44
AmrFiqi <amr.h.elfiqi@gmail.com>
2023-04-09 14:50:05 +0200

    Setting up MVC architecture

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    // MARK: - IBAction Methods
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    // MARK: - Class Methods
+    
+
+}
+
